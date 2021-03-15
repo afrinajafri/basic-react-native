22aa19babe2fd73d579a1e6b668d1612cbb0722c
afrinajafri <afrinajafri@gmail.com>
2021-03-15 16:14:04 +0800

    MyApp

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+// import { NavigationContainer } from '@react-navigation/native'
+// import 'react-native-gesture-handler';
 import React, { Component } from 'react'
 import { View, Text, StyleSheet, Image,
   Pressable, Button, TextInput, KeyboardAvoidingView, Platform,
@@ -5,111 +7,22 @@ import { View, Text, StyleSheet, Image,
 
  } from 'react-native'
 
-import Cell from './src/components/Cell'
+import MyApp from './src/MyApp'
 
 class App extends Component {
 
-  constructor(props){
-
-    super(props);
-
-    this.state = {
-      text: '',
-    }
-
-  }  
-
-
   render(){
-    return (     
-      <View style={styles.container} >
-      <KeyboardAvoidingView
-       behavior={Platform.OS === "ios" ? "padding" : "height"}      
-     >
-      <ScrollView>
-      <Text>
-        {this.state.text}
-      </Text>
 
-      <Cell title={"ABC 1"} />
-      <Cell title={"ABC 1"} />
-      <Cell title={"ABC 1"} />
-      <Cell title={"ABC 1"} />
-      <Cell title={"ABC 1"} />
-      <Cell title={"ABC 1"} />
-      <Cell title={"ABC 1"} />
-      <Cell title={"ABC 1"} />
-      
+    return(
+      // <NavigationContainer>
+    <MyApp>
 
-      <TextInput 
-        style={{
-          padding:8,
-          // backgroundColor:'skyblue',
-          marginVertical: 6,
-          borderColor: 'grey',
-          borderStyle: 'solid',
-          borderWidth: 1,
-        }}
-
-        placeholder="Input"
-
-        onChangeText={(text)=>{
-          this.setState({
-            text: text,
-          })
-        }}
-
-      />
-
-       
-      <Pressable onPress={()=>{
-        alert(this.state.text);
-
-      }
-      }>
-
-      <Button
-        onPress={()=>{}}
-        title="Learn More"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />  
-
-      <Text>I'm pressable!</Text>
-      </Pressable>
-
-      </ScrollView>
-      </KeyboardAvoidingView>
-      </View>
+    </MyApp>
+    // </NavigationContainer>
     )
+    
   }
-
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: "#eaeaea"
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold"
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-});
+ 
 
 export default App;
